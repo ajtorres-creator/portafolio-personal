e28b720e4d1902f2c3271b9d98ccd25924938d62
ajtorres-creator <aj.torres@eston.ecotec.edu.ec>
2026-08-30 11:55:21 -0500

    Agregamos modo oscuro y LocalStorage

--- script.js
+++ script.js
@@ -29,3 +29,24 @@ formulario.addEventListener("submit", function(event){
     event.preventDefault();
     validarFormulario();
 });
+
+const botonTema = document.getElementById("boton-tema");
+function CambiarTema () {
+
+    document.body.classList.toggle("modo-oscuro");
+
+    if (document.body.classList.contains("modo-oscuro")){
+        localStorage.setItem("tema", "oscuro");
+        botonTema.textContent = "Modo claro";
+    } else {
+        localStorage.setItem("tema", "claro");
+        botonTema.textContent = "Modo oscuro";
+    }
+}
+botonTema.addEventListener("click", CambiarTema);
+const temaGuardado = localStorage.getItem("tema");
+
+if(temaGuardado === "oscuro") {
+    document.body.classList.add("modo oscuro");
+    botonTema.textContent ="modo claro";
+}
