e2d149e190b395704bdc9ba7281c4e586a0b0a9f
ajtorres-creator <aj.torres@eston.ecotec.edu.ec>
2026-08-30 11:31:18 -0500

    Agregamos validacion al formulario

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ const imagenPerfil = document.getElementById("imagen-perfil");
 imagenPerfil.addEventListener("mouseover", function() {
     mensaje.textContent = "¡Estas viendo mi foto!";
 });
+
+const formulario = document.querySelector("form");
+function validarFormulario(){
+    const nombre = document.getElementById("nombre").value;
+    const correo = document.getElementById("correo").value;
+    const mensajeFormulario = document.getElementById("mensaje").value;
+
+    if(nombre === "" || correo === "" || mensajeFormulario === "") {
+        alert("Por favor, complete los campos.");
+        return false;
+    } else {
+        alert ("¡Formulario enviado exitosamente!");
+        return true;
+    }
+}
+
+formulario.addEventListener("submit", function(event){
+    event.preventDefault();
+    validarFormulario();
+});
