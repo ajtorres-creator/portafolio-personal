318996ca9b2c8a018f381732c18c4b0a3cb1fbd8
ajtorres-creator <aj.torres@eston.ecotec.edu.ec>
2026-08-30 11:14:24 -0500

    Implementamos eventos interactivos

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("JavaScript conectado")
+const botonMensaje = document.getElementById("boton-mensaje");
+const mensaje = document.getElementById("mensaje-js");
+
+botonMensaje.addEventListener("click" , function() {
+    mensaje.textContent = "¡Gracias por su visita!";
+});
+
+const imagenPerfil = document.getElementById("imagen-perfil");
+imagenPerfil.addEventListener("mouseover", function() {
+    mensaje.textContent = "¡Estas viendo mi foto!";
+});
